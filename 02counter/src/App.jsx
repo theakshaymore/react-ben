cc800701525fca7cddde6571c757ab9e42925c71
Akshay More <moreakshay725@gmail.com>
2025-07-14 23:05:26 +0530

    Update counter display to reflect state changes in App component

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,9 +2,16 @@ import React, { useState } from "react";
 
 function App() {
   let [counter, setCounter] = useState(15);
+
   return (
     <>
-      <h1>Ben Counter 15</h1>
+      <h1>Ben Counter {counter}</h1>
+
+      <button>Increment {counter}</button>
+      <br />
+      <button>Decrement {counter}</button>
+
+      <footer>{counter}</footer>
     </>
   );
 }
