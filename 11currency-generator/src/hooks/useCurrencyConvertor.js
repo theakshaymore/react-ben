d13748504c5e104b13ecbc4f8362406aff8cc331
Akshay More <moreakshay725@gmail.com>
2025-07-19 21:35:30 +0530

    fixed custom hooks

--- 11currency-generator/src/hooks/useCurrencyConvertor.js
+++ 11currency-generator/src/hooks/useCurrencyConvertor.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useCurrencyConvertor(currency) {
   const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.rates));
 
-  fetch(`https://open.er-api.com/v6/latest/${currency}`)
-    .then((res) => res.json())
-    .then((res) => setData(res.rates));
+    console.log(data);
+  }, currency);
 
   console.log(data);
   return data;
