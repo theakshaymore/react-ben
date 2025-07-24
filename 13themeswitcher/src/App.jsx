75135bee2dd7460b01f4d8a7f052ef50075889a1
Akshay More <moreakshay725@gmail.com>
2025-07-25 01:02:04 +0530

    toggle logic

--- 13themeswitcher/src/App.jsx
+++ 13themeswitcher/src/App.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* {Theme btn} */}
-        </div>
+  const [themeMode, setThemeMode] = useState("light");
 
-        <div className="w-full max-w-sm mx-auto">{/* {Card } */}</div>
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* {Theme btn} */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* {Card } */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
