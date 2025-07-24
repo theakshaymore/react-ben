9e7a5b9343dea82d451ca815eed1d16f52fa5503
Akshay More <moreakshay725@gmail.com>
2025-07-25 01:25:15 +0530

    workaround through index.css file

--- 13themeswitcher/src/App.jsx
+++ 13themeswitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light");
@@ -23,10 +25,12 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* {Theme btn} */}
+            <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* {Card } */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
