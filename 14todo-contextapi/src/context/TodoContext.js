2ea57cb619fc72996de45d867435b035523c62f8
Akshay More <moreakshay725@gmail.com>
2025-07-26 15:15:48 +0530

    Add initial state and methods to TodoContext

--- 14todo-contextapi/src/context/TodoContext.js
+++ 14todo-contextapi/src/context/TodoContext.js
@@ -1,7 +1,19 @@
 import { createContext, useContext } from "react";
 
 // context creation with initial values
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      title: "todo title",
+      completed: false,
+    },
+  ],
+  addTodo: (title) => {},
+  editTodo: (id, title) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
 // context-provider creation
 export const TodoProvider = TodoContext.Provider;
