3019d66bbd0a1be4620912d32ea527809aadf83c
Akshay More <moreakshay725@gmail.com>
2025-08-15 15:26:35 +0530

    hasProperty

--- 33-js-base/hitesh/ownProps.js
+++ 33-js-base/hitesh/ownProps.js
@@ -1,2 +1,10 @@
+Array.prototype.myProperty = "akshay";
+let myArray = [1, 2, 3, 4, 5];
 
+// console.log(myArray);
 
+for (let i in myArray) {
+  if (myArray.hasOwnProperty(i)) {
+    console.log(i);
+  }
+}
