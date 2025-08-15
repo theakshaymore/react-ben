1941441060040133984c3fee5c2cd96cd14dd26b
Akshay More <moreakshay725@gmail.com>
2025-08-15 18:36:50 +0530

    refactor: rename forEachOne function and maintain forEachTwo functionality

--- 33-js-base/hitesh/customForEach.js
+++ 33-js-base/hitesh/customForEach.js
@@ -1,6 +1,19 @@
 // level 1
-Array.prototype.forEachTwo = function (callback) {
+Array.prototype.forEachOne = function (callback) {
   for (let i = 0; i < this.length; i++) {
     callback(this[i], this);
+  }
+};
+
+// level 2
+Array.prototype.forEachTwo = function (callback, thisContext) {
+  if (typeof callback != "function") {
+    throw `not a function`;
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    if (this.hasOwnProperty) {
+      callback.call(thisContext, this[i], i);
+    }
   }
 };
