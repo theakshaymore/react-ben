c1d0b0d476266bb06f6238e06d64d58d0f77f29f
Akshay More <moreakshay725@gmail.com>
2025-08-17 19:57:42 +0530

    final code api

--- api-manipulation/src/OnlineAPI.jsx
+++ api-manipulation/src/OnlineAPI.jsx
@@ -17,32 +17,15 @@ function OnlineAPI() {
   };
   return (
     <div>
-      <h2 className="text-2xl font-semibold">User List from API</h2>
-      <h3 className="text-sm text-gray-600 mb-4">User Count: {users.length}</h3>
-      <button
-        onClick={getUsersFromAPI}
-        className="mb-6 rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-500"
-      >
-        Get UserList
-      </button>
+      <h2>User List from API</h2>
+      <h3>User Count: {users.length}</h3>
+      <button onClick={getUsersFromAPI}>Get UserList</button>
 
       {users.map((user) => (
-        <div
-          className="max-w-sm rounded overflow-hidden shadow-lg mb-6"
-          key={user.id}
-        >
-          <img
-            className="w-full h-30 object-cover"
-            src={user.picture.medium}
-            alt="User picture"
-            loading="lazy"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">
-              <p className="truncate">{user.email}</p>
-              <p className="text-gray-600">{user.phone}</p>
-            </div>
-          </div>
+        <div key={user.id}>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <img src={user.picture.medium} alt="user-image" />
         </div>
       ))}
     </div>
