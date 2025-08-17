0e3839088781b31141ff933329b1c26d3a494a0c
Akshay More <moreakshay725@gmail.com>
2025-08-17 20:07:13 +0530

    onchange setup

--- api-manipulation/src/OnlineAPI.jsx
+++ api-manipulation/src/OnlineAPI.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 function OnlineAPI() {
   const [users, setUsers] = useState([]);
+
   const getUsersFromAPI = async () => {
     try {
       const response = await axios.get(
@@ -15,12 +16,30 @@ function OnlineAPI() {
       console.log(error);
     }
   };
+
+  const changeHandler = async (event) => {
+    event.preventDefault();
+    const query = event.target.value;
+
+    const response = await axios.get(
+      `https://api.freeapi.app/api/v1/public/randomusers?q=${query}`
+    );
+
+    console.log(response.data?.data?.data);
+  };
   return (
     <div>
       <h2>User List from API</h2>
       <h3>User Count: {users.length}</h3>
       <button onClick={getUsersFromAPI}>Get UserList</button>
-
+      <div>
+        <input
+          type="text"
+          placeholder="enter text"
+          className="border p-3"
+          onChange={changeHandler}
+        />
+      </div>
       {users.map((user) => (
         <div key={user.id}>
           <p>{user.email}</p>
