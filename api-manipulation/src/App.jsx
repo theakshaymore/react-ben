dcde94d149e92ec3c61e3467d83243ebddd27f20
Akshay More <moreakshay725@gmail.com>
2025-08-17 16:28:18 +0530

    proxy for backend

--- api-manipulation/src/App.jsx
+++ api-manipulation/src/App.jsx
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsersFromAPI = async () => {
+    try {
+      const response = await axios.get("/api/jokes");
+      setUsers(response.data);
+      // console.log(response.data);
+      console.log(users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h1>Ben APIs</h1>
+      <div>
+        <h2>User List from API</h2>
+        <h3>User Count: {users.length}</h3>
+        <button onClick={getUsersFromAPI}>Get UserList</button>
+        {users.map((user) => (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.desc}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
