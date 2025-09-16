6714034f22bbcb2612f07225dc4eacae42bdff7e
Akshay More <moreakshay725@gmail.com>
2025-09-17 00:25:51 +0530

    pagination fix

---
+++ Interview-Questions/react-pagination/src/App.jsx
@@ -0,0 +1,11 @@
+import Pagination from "./components/Pagination";
+
+export default function App() {
+  return <Pagination />;
+}
+
+// await fetch("https://jsonplaceholder.typicode.com/photos").then((res) =>
+//   setProducts(res.json())
+// );
+//1
+// main
