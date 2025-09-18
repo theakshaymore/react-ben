cd041e677320f338924cb780492cdb745e14e857
Akshay More <moreakshay725@gmail.com>
2025-09-19 01:37:03 +0530

    pagination logic

--- Interview-Questions/react-pagination/src/components/Pagination.jsx
+++ Interview-Questions/react-pagination/src/components/Pagination.jsx
@@ -2,20 +2,26 @@ import { useEffect, useState } from "react";
 import ProductCard from "../components/PageCard";
 // import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 6;
 
 const Pagination = () => {
   const [products, setProducts] = useState([]);
+  const [currentPageNo, setCurrentPageNo] = useState(0);
+
+  const getProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProducts(data);
+  };
 
   useEffect(() => {
-    const getProducts = async () => {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProducts(data);
-      console.log(products);
-    };
     getProducts();
   }, []);
+
+  let productsLength = products.length;
+  let noOfPages = Math.ceil(productsLength / PAGE_SIZE);
+  let start = currentPageNo * PAGE_SIZE;
+  let end = start + PAGE_SIZE;
 
   return (
     <div>
@@ -25,7 +31,7 @@ const Pagination = () => {
         <div className="main-container">
           <h1>Pagination</h1>
           <div className="products-grid">
-            {products.map((product) => (
+            {products.slice(start, end).map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
