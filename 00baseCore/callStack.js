2e1679c1f12b4010ba1d7c47a919efddbefa9fe1
Akshay More <moreakshay725@gmail.com>
2025-09-21 20:53:48 +0530

    call stack

---
+++ 00baseCore/callStack.js
@@ -0,0 +1,14 @@
+var a = 5;
+
+function run(num) {
+  console.log(b);
+  var ans = num * num;
+  return ans;
+}
+
+var runOne = run(a);
+var b = 9;
+// var runTwo = run(b);
+
+console.log(runOne);
+// console.log(runTwo);
