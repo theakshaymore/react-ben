e19cebd153df26b95d66e267182e4c38cb9658b0
Akshay More <moreakshay725@gmail.com>
2025-09-21 23:42:15 +0530

    Refactor Person constructor to ES6 class syntax and remove commented-out legacy code

--- 00baseCore/objects.js
+++ 00baseCore/objects.js
@@ -10,17 +10,35 @@
 // console.log(data);
 
 // Functional Constructor
-function Person(fname, lname, contact) {
-  this.fname = fname;
-  this.lname = lname;
-  this.contact = contact;
+// function Person(fname, lname, contact) {
+//   this.fname = fname;
+//   this.lname = lname;
+//   this.contact = contact;
 
-  this.getData = function () {
-    console.log(this.fname + " " + this.lname);
-  };
+//   this.getData = function () {
+//     console.log(this.fname + " " + this.lname);
+//   };
+// }
+
+// Es6
+
+class Person {
+  constructor(fname, lname, contact) {
+    this.fname = fname;
+    this.lname = lname;
+    this.contact = contact;
+  }
+
+  getContact() {
+    console.log(this.contact);
+  }
+
+  getDetails() {
+    console.log(this.fname, this.lname);
+  }
 }
 
 const p1 = new Person("akshay", "more", "8888888");
 const p2 = new Person("john", "more", "9999999");
 
-console.log(p1.getData());
+console.log(p1);
