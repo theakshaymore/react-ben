76c070023dcdb25a4e00632c914147cbeab05e63
Akshay More <moreakshay725@gmail.com>
2025-09-22 19:51:58 +0530

    Enhance logging in inheritance example to show before and after state of prototype property

--- 00baseCore/inheritance.js
+++ 00baseCore/inheritance.js
@@ -7,4 +7,7 @@ const p1 = {
 };
 
 const p2 = Object.create(p1);
-console.log(p2.getData());
+console.log("before: ", p2.getData());
+p2.__proto__.fname = "new";
+console.log("after: ", p2.getData());
+console.log("after: ", p1.getData());
