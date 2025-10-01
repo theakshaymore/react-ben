b1f2aa0689effb56968c3fa5eddfa52b1714b0a0
Akshay More <moreakshay725@gmail.com>
2025-10-01 14:49:15 +0530

    Add comments and separate sections in functions.js for clarity on variable scope

--- 00advance-js/functions.js
+++ 00advance-js/functions.js
@@ -18,7 +18,19 @@ function run() {
 
 run();
 
+// ------------------------------------------------
+
 var a = 10;
 var a = 6;
 
 console.log(a);
+
+// -------------------------------------------------------
+
+// var in global scope creates a property on window (in browsers)
+var x = 10;
+console.log(window.x); // 10
+
+// let does NOT create a window property
+let x = 10;
+console.log(window.x); // undefined
