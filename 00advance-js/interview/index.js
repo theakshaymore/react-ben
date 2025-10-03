36dffa96bea9675259978ed24338b88703395b46
Akshay More <moreakshay725@gmail.com>
2025-10-03 16:14:21 +0530

    Refactor run function to use a loop with setTimeout for sequential logging of indices

--- 00advance-js/interview/index.js
+++ 00advance-js/interview/index.js
@@ -1,10 +1,37 @@
+//
 function run() {
-  var a = 10;
-  setTimeout(function () {
-    console.log(a);
-  }, 1000);
+  for (let i = 0; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
 
   console.log("statement .......");
 }
 
 run();
+
+// {
+// var a = 5
+// // setimeout(console.log(a);6
+// // setimeout(console.log(a);6
+// // setimeout(console.log(a);6
+// // setimeout(console.log(a);6
+// }
+
+// {
+//   let a = 0
+//   setimeout(console.log(a);
+// }
+// {
+//   let a = 5
+//   setimeout(console.log(a);
+// }
+// {
+//   let a = 5
+//   setimeout(console.log(a);
+// }
+// {
+//   let a = 5
+//   // setimeout(console.log(a);
+// }
