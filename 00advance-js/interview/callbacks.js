dd9e6b740525647e52377081c93fc6bed673fd14
Akshay More <moreakshay725@gmail.com>
2025-10-06 13:07:37 +0530

    callbacks with deb

---
+++ 00advance-js/interview/callbacks.js
@@ -0,0 +1,19 @@
+// A callback is simply a function that is passed as an argument to another function,
+// with the expectation that it will be "called back" (executed) at a later time
+
+setTimeout(() => {
+  console.log("Set timeout....");
+}, 2000);
+
+function a(b) {
+  console.log("Function A");
+  b();
+}
+
+a(function b() {
+  console.log("Function B");
+});
+
+document.getElementById("btn").addEventListener("click", function run() {
+  console.log("Button Clicked....");
+});
