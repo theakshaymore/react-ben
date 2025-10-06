bcda8fbd1d3dbe551dc22b9d519ea496063b3b2a
Akshay More <moreakshay725@gmail.com>
2025-10-06 20:55:11 +0530

    Add: Separate sections in arrayMethods.js with visual dividers

--- 00advance-js/interview/22__arrayMethods.js
+++ 00advance-js/interview/22__arrayMethods.js
@@ -5,11 +5,15 @@ let arr = [1, 9, 5, 7, 8];
 
 // const res = arr.map((i) => console.log(i));
 
+// -------------------------------------------------
+
 // filter
 const res = arr.filter((i) => i % 2 == 0);
 const res2 = arr.filter((i) => i > 5);
 
 // console.log(res2);
+
+// -------------------------------------------------
 
 // reduce
 
