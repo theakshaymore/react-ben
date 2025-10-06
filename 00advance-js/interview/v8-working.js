fffaff73b0b5a8daec0270b60eca0102c838e20b
Akshay More <moreakshay725@gmail.com>
2025-10-06 16:27:49 +0530

    v8 engine working

---
+++ 00advance-js/interview/v8-working.js
@@ -0,0 +1,11 @@
+// V8 Engine by Google working
+// 1. Javascript source code
+// -
+// 2. code parser
+// -
+// 3. AST-(Abstract Syntax Tree)
+// -
+// 4. Interpreter
+// 4.5 Compiler
+
+// 5. bytecode to run by machine
