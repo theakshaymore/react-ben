6faf716be1a77052d62cd3645c8ff0049510a5d1
Akshay More <moreakshay725@gmail.com>
2025-10-06 17:12:26 +0530

    async await implementation

--- 00advance-js/interview/20__blocking.js
+++ 00advance-js/interview/20__blocking.js
@@ -10,22 +10,37 @@
 // console.log(currentDate);
 
 // Using Promise
-console.log("Start");
-let count;
+// console.log("Start");
+// let count;
 
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    count = 30;
-    resolve(count);
-    reject("Not able to....");
-  }, 3000);
-})
-  .then((count) => {
-    console.log(count);
-    console.log("End");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     count = 30;
+//     resolve(count);
+//     reject("Not able to....");
+//   }, 3000);
+// })
+//   .then((count) => {
+//     console.log(count);
+//     console.log("End");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 //   Using async await
+
+console.log("start");
+let count;
+
+async function run() {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      count = 99;
+      resolve(count);
+      //  reject("Error.......");
+    }, 3000);
+  });
+  console.log("end");
+}
+run();
