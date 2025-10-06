4e26c77539d31085053d411d2986258d78e98552
Akshay More <moreakshay725@gmail.com>
2025-10-06 17:07:40 +0530

    promise implementation

--- 00advance-js/interview/20__blocking.js
+++ 00advance-js/interview/20__blocking.js
@@ -1,8 +1,31 @@
-console.log("Start");
-setTimeout(() => {
-  console.log("settimeout");
-}, 3000);
-console.log("End");
+// console.log("Start");
+// let count;
+// setTimeout(() => {
+//   console.log("settimeout");
+//   count = 30;
+// }, 3000);
+// console.log("End:", count);
 
-let currentDate = new Date().getTime();
-console.log(currentDate);
+// let currentDate = new Date().getTime();
+// console.log(currentDate);
+
+// Using Promise
+console.log("Start");
+let count;
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    count = 30;
+    resolve(count);
+    reject("Not able to....");
+  }, 3000);
+})
+  .then((count) => {
+    console.log(count);
+    console.log("End");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//   Using async await
