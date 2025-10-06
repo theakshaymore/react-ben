acf5134f8d8295e7afff4f67fb29a4506627fcde
Akshay More <moreakshay725@gmail.com>
2025-10-06 20:29:23 +0530

    filter method

--- 00advance-js/interview/22__arrayMethods.js
+++ 00advance-js/interview/22__arrayMethods.js
@@ -1,4 +1,9 @@
 // map
 arr = [1, 2, 5, 7, 8];
 
-const res = arr.map((i) => console.log(i));
+// const res = arr.map((i) => console.log(i));
+
+// filter
+const res = arr.filter((i) => i % 2 == 0);
+
+console.log(res);
