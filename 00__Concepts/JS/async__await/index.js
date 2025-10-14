2b4b1b770c63d63fc44c4a46f3fb3a0f557e8057
Akshay More <moreakshay725@gmail.com>
2025-10-14 12:53:39 +0530

    async await example

--- 00__Concepts/JS/async__await/index.js
+++ 00__Concepts/JS/async__await/index.js
@@ -10,5 +10,21 @@ async function main() {
   const result = await run();
   console.log(result); // "running......"
 }
+// main();
 
-main();
+// .............................................................
+
+const myPromise = new Promise((resolve, reject) => {
+  resolve("promise resolved......");
+  reject("promise rejected......");
+});
+
+async function handlePromise() {
+  const response = await myPromise;
+  return response;
+  //   console.log(response);
+}
+
+handlePromise()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
