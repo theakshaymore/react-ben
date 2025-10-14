7161380c7fc6f34204baa320f9ad081adf1e4a8b
Akshay More <moreakshay725@gmail.com>
2025-10-14 08:08:41 +0530

    promise code

--- 00__new/promises/promise.js
+++ 00__new/promises/promise.js
@@ -6,3 +6,23 @@
 // -- fullfilled - operation susscessful
 // -- rejected - operation failed
 // -- prmise can be only change from pending to other state once - pending -> fulfilled or pending -> rejected - only once
+
+function addDataToDB(data) {
+  let res = false;
+  return res;
+}
+
+const promise = new Promise((fulfilled, rejected) => {
+  let data = 5;
+  if (addDataToDB(data)) {
+    fulfilled(data);
+  } else rejected("operation failed");
+});
+
+promise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
