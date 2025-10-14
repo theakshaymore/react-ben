eae70a82f1dbf10dd7cde6e90de56a7926cd6d2c
Akshay More <moreakshay725@gmail.com>
2025-10-14 10:00:42 +0530

    aaa

--- 00__new/promises/promise.js
+++ 00__new/promises/promise.js
@@ -38,7 +38,7 @@ myPromise
   .then((res) => {
     return res.json();
   })
-  .then((data) => console.log(data))
-  .catch((err) => {
-    console.log(err);
-  });
+  .then((data) => console.log(data));
+//   .catch((err) => {
+//     console.log(err);
+//   });
