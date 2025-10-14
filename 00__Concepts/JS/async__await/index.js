380430d5570bad94ec902ec06023243133e49db5
Akshay More <moreakshay725@gmail.com>
2025-10-14 20:17:15 +0530

    aass

--- 00__Concepts/JS/async__await/index.js
+++ 00__Concepts/JS/async__await/index.js
@@ -14,20 +14,32 @@ async function main() {
 
 // .............................................................
 
-const myPromise = new Promise((resolve, reject) => {
+const myPromise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("promise resolved......");
-  }, 5000);
+    resolve("promise1 resolved......");
+  }, 10000);
+});
 
-  //   reject("promise rejected......");
+const myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise2 resolved......");
+  }, 5000);
 });
 
 async function handlePromise() {
-  console.log("before promise.....");
-  const response = await myPromise;
-  //   return response;
-  console.log(response);
-  console.log("after promise.....");
+  console.log("before promise starts.....");
+  //   const response1 = await myPromise1;
+  //   console.log("promise 1 done executing!");
+  //   console.log(response1);
+
+  //   const response2 = await myPromise2;
+  //   console.log("promise 2 done executing!");
+  //   console.log(response2);
+
+  const [response1, response2] = await Promise.all([myPromise1, myPromise2]);
+
+  console.log(response1);
+  console.log(response2);
 }
 
 handlePromise();
