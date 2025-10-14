38d2f6c40ab24863c295eb4d22fc4a28e8e17cc7
Akshay More <moreakshay725@gmail.com>
2025-10-14 08:01:17 +0530

    promise notes

---
+++ 00__new/promises/promise.js
@@ -0,0 +1,8 @@
+// Promise
+// - it is an object, it is way to handle/manage asynchronous code in js compared to callbacks
+// - avoids callback hell
+// - states of promise
+// -- pending - initial state
+// -- fullfilled - operation susscessful
+// -- rejected - operation failed
+// -- prmise can be only change from pending to other state once - pending -> fulfilled or pending -> rejected - only once
