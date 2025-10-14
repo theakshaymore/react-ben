d3aa05931ac553073720760ceb8c6cbb51e5a56c
Akshay More <moreakshay725@gmail.com>
2025-10-14 11:11:35 +0530

    promise all example

---
+++ 00__Concepts/JS/promises/promiseAll.js
@@ -0,0 +1,34 @@
+function getUser() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("✅ User fetched");
+      resolve("John");
+    }, 1000);
+  });
+}
+
+function getPosts() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("✅ Posts fetched");
+      resolve(["Post 1", "Post 2"]);
+    }, 4000);
+  });
+}
+
+function getComments() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("✅ Comments fetched");
+      resolve(["Comment 1", "Comment 2"]);
+    }, 1500);
+  });
+}
+
+console.log("Starting all requests.....");
+
+Promise.all([getUser(), getPosts(), getComments()]).then((res) => {
+  console.log(res[0]);
+  console.log(res[0]);
+  console.log(res[0]);
+});
