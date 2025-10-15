a3f059c5c17545350fd54a32190958c690eb2323
Akshay More <moreakshay725@gmail.com>
2025-10-15 16:28:36 +0530

    call method

--- 00__Concepts/JS/this/this.js
+++ 00__Concepts/JS/this/this.js
@@ -1,6 +1,6 @@
-"use strict";
+// "use strict";
 
-console.log(this);
+// console.log(this);
 
 // a = "string";
 
@@ -9,7 +9,24 @@ console.log(this);
 // console.log(this.navigator.userAgent);
 
 function run() {
+  // depends on strict mode
   console.log(this);
 }
 
-run();
+// run();
+
+const obj1 = {
+  name: "akshay",
+  runFunction: function () {
+    console.log(this.name);
+  },
+};
+
+obj1.runFunction();
+
+const obj2 = {
+  name: "rohit",
+};
+
+// IMP: call()
+obj1.runFunction.call(obj2);
