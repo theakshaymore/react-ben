cad52b80db455fed2a23bdebbed3876f5d145b79
Akshay More <moreakshay725@gmail.com>
2025-10-15 21:04:35 +0530

    pagination code

--- 00__Concepts/React/pagination/src/App.jsx
+++ 00__Concepts/React/pagination/src/App.jsx
@@ -3,24 +3,38 @@ import React, { useState, useEffect } from "react";
 // import "./App.css";
 
 const API_URL = "https://fakestoreapi.com/products/";
+const PAGE_SIZE = 3;
 
 function App() {
   const [data, setData] = useState([]);
+  const [currentPageNo, setCurrentPageNo] = useState(0);
+  // const [start, setStart] = useState(0);
+  // const [end, setEnd] = useState(0);
 
   useEffect(() => {
     handleFetchData();
   }, []);
 
   async function handleFetchData() {
-    await fetch(API_URL)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      })
-      .catch((err) => console.log("FAILED IN FETCH!: ", err));
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error("FAILED IN FETCH!: ", err);
+    }
+  }
+
+  const start = currentPageNo * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  function handlePageClick(number) {
+    setCurrentPageNo(number);
+  }
+
+  function setPagination() {
+    start = currentPageNo * PAGE_SIZE;
+    end = start + PAGE_SIZE;
   }
 
   return (
@@ -30,9 +44,16 @@ function App() {
           <h1 className="text-3xl sm:text-4xl font-bold text-center text-gray-100 mb-8">
             Product Showcase
           </h1>
-
+          <div className="text-gray-400 text-sm mt-1 flex flex-wrap gap-6 justify-center mb-8">
+            <div onClick={() => handlePageClick(0)}>0</div>
+            <div onClick={() => handlePageClick(1)}>1</div>
+            <div onClick={() => handlePageClick(2)}>2</div>
+            <div onClick={() => handlePageClick(3)}>3</div>
+            <div onClick={() => handlePageClick(4)}>4</div>
+            <div onClick={() => handlePageClick(5)}>5</div>
+          </div>
           <div className="flex flex-wrap gap-6 justify-center">
-            {data.map((product) => (
+            {data.slice(start, end).map((product) => (
               <div
                 key={product.id}
                 className="w-full flex-grow sm:flex-grow-0 sm:w-64 bg-gray-800 rounded-lg border border-gray-700 overflow-hidden transform hover:scale-105 transition-transform duration-300 ease-in-out"
