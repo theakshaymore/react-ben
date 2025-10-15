4a7f15b9a300d91f106bc0a0ea775f94295ffcaa
Akshay More <moreakshay725@gmail.com>
2025-10-15 19:39:17 +0530

    this done

--- 00__Concepts/JS/this/this.js
+++ 00__Concepts/JS/this/this.js
@@ -15,18 +15,49 @@ function run() {
 
 // run();
 
+// ...............................
+
 const obj1 = {
   name: "akshay",
   runFunction: function () {
-    console.log(this.name);
+    console.log(this);
   },
 };
 
-obj1.runFunction();
+// obj1.runFunction();
 
 const obj2 = {
   name: "rohit",
 };
 
 // IMP: call()
-obj1.runFunction.call(obj2);
+// obj1.runFunction.call(obj2);
+
+// ------------------------------
+
+const student = {
+  firstname: "akshayz",
+  getname: function () {
+    const run = () => {
+      console.log(this);
+    };
+    run();
+  },
+};
+
+// student.getname();
+
+// ---------------------------------------
+
+function collage() {
+  this.teacher = "ss";
+
+  setTimeout(() => {
+    this.teacher = "hhhh";
+    console.log(this.teacher);
+  }, 2000);
+}
+
+const myCollage = new collage();
+
+myCollage();
