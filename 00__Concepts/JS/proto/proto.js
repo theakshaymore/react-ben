381500994bf50e9c2c174e61b61430335e08a79a
Akshay More <moreakshay725@gmail.com>
2025-10-16 08:38:04 +0530

    proto1

--- 00__Concepts/JS/proto/proto.js
+++ 00__Concepts/JS/proto/proto.js
@@ -1,1 +1,22 @@
+const student1 = {
+  fname: "saket",
+  lname: "more",
+  getFullname() {
+    return `${this.fname} ${this.lname}`;
+  },
+};
 
+// const student2 = Object.create(student1);
+const student2 = {};
+student2.__proto__ = student1;
+
+// console.log(student2.getFullname());
+// console.log(student2.fname);
+
+// console.log(student1.__proto__);
+
+let fullname = "akshay";
+
+// let name = new String('akshay')
+
+console.log(fullname.__proto__.__proto__.__proto__);
