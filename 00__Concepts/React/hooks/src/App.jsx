736cfd668efd1d893b62991648ad7e15828a595a
Akshay More <moreakshay725@gmail.com>
2025-10-19 13:31:02 +0530

    REACT: useState counter demo

--- 00__Concepts/React/hooks/src/App.jsx
+++ 00__Concepts/React/hooks/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import UseStateDemo from "./components/UseStateDemo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Ben Hooks</h1>
+      <h1>Ben Hooks Demo</h1>
+      <UseStateDemo />
     </>
   );
 }
--- 00__Concepts/React/hooks/src/components/UseStateDemo.jsx
+++ 00__Concepts/React/hooks/src/components/UseStateDemo.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UseStateDemo() {
+  //
+  const [count, setCount] = useState(0);
+
+  function handleChange() {
+    setCount((prev) => prev + 1);
+    // setCount((prev) => prev + 1);
+    // setCount((prev) => prev + 1);
+    // setCount((prev) => prev + 1);
+    // setCount((prev) => prev + 1);
+  }
+
   return (
     <>
+      <hr />
       <h2>useState()</h2>
+      <p>{count}</p>
+      <button onClick={handleChange}>increase count</button>
     </>
   );
 }
