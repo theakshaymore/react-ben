a80b90e85625d6ea67b2297d8a90d7cde872cc02
Akshay More <moreakshay725@gmail.com>
2025-10-19 14:50:52 +0530

    REACT: form data handling in switch case

--- 00__Concepts/React/hooks/src/components/UseStateDemo.jsx
+++ 00__Concepts/React/hooks/src/components/UseStateDemo.jsx
@@ -4,6 +4,12 @@ function UseStateDemo() {
   //
   const [count, setCount] = useState(0);
 
+  const [user, setUser] = useState({
+    username: "akshaymore",
+    age: 20,
+    email: "akshay@gmail.com",
+  });
+
   function handleChange() {
     setCount((prev) => prev + 1);
     // setCount((prev) => prev + 1);
@@ -12,12 +18,65 @@ function UseStateDemo() {
     // setCount((prev) => prev + 1);
   }
 
+  function handleUserChange(fieldName, fieldValue) {
+    switch (fieldName) {
+      case "username":
+        setUser({ ...user, username: fieldValue });
+        break;
+      case "age":
+        setUser({ ...user, age: fieldValue });
+        break;
+      case "email":
+        setUser({ ...user, email: fieldValue });
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <>
       <hr />
       <h2>useState()</h2>
       <p>{count}</p>
       <button onClick={handleChange}>increase count</button>
+      <div>
+        <input
+          type="text"
+          name="username"
+          placeholder="enter new username"
+          value={user.username}
+          onChange={(event) =>
+            handleUserChange(event.target.name, event.target.value)
+          }
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="enter new age"
+          value={user.age}
+          onChange={(event) =>
+            handleUserChange(event.target.name, event.target.value)
+          }
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="enter new email"
+          value={user.email}
+          onChange={(event) =>
+            handleUserChange(event.target.name, event.target.value)
+          }
+        />
+        <br />
+        <button type="submit">update user data</button>
+        <br />
+        <ul>
+          <li>{user.username}</li>
+          <li>{user.age}</li>
+          <li>{user.email}</li>
+        </ul>
+      </div>
     </>
   );
 }
