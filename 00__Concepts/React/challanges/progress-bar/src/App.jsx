2e817f4984cfb859546ecf67dc3f6334f33b246d
Akshay More <moreakshay725@gmail.com>
2025-10-21 15:13:04 +0530

    REACT: progress bar done

--- 00__Concepts/React/challanges/progress-bar/src/App.jsx
+++ 00__Concepts/React/challanges/progress-bar/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [progress, setProgress] = useState(0);
+
+  function promise1() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("promise1 done....");
+      }, 3000);
+    });
+  }
+  function promise2() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("promise2 done....");
+      }, 5000);
+    });
+  }
+
+  function promise3() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("promise3 done....");
+      }, 3000);
+    });
+  }
+
+  useEffect(() => {
+    const loadData = async () => {
+      setProgress(0);
+      const result1 = await promise1();
+      setProgress(30);
+      console.log(result1);
+
+      const result2 = await promise2();
+      setProgress(70);
+      console.log(result2);
+
+      const result3 = await promise3();
+      setProgress(100);
+      console.log(result3);
+    };
+    loadData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Ben progress bar</h1>
+      <div className="progressbar">
+        <div className="progressbar__line" style={{ width: `${progress}%` }}>
+          {progress}%
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
