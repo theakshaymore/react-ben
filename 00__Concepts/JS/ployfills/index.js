8fdd1551bc23caf1c81c32e7bb7dbdeb4b408b0a
Akshay More <moreakshay725@gmail.com>
2025-11-01 08:44:08 +0530

    JS: call and bind example

--- 00__Concepts/JS/ployfills/index.js
+++ 00__Concepts/JS/ployfills/index.js
@@ -9,9 +9,45 @@ function greet(greeting, desgination, companyName) {
   );
 }
 
-greet.call(person, "Hello", "Developer", "Google");
+// greet.call(person, "Hello", "Developer", "Google");
 
-greet.apply(person, ["Hi", "Engineer", "Intel"]);
+// greet.apply(person, ["Hi", "Engineer", "Intel"]);
 
-const bindRun = greet.bind(person, "Hello", "Developer", "Microsoft");
-bindRun();
+// const bindRun = greet.bind(person, "Hello", "Developer", "Microsoft");
+// bindRun();
+
+// -----------------------------------------------------------------------------------------
+// bind()
+
+const student = {
+  name: "akshay",
+  getName() {
+    return this.name;
+  },
+};
+
+const output = student.getName;
+// console.log(output());
+
+const output2 = output.bind(student);
+// console.log(output2());
+
+// ---------------------------------------------------------------------------------------
+// call()
+
+const person1 = {
+  name: "Rahul",
+  age: 25,
+};
+
+const person2 = {
+  name: "Priya",
+  age: 30,
+};
+
+function introduce() {
+  console.log(`I'm ${this.name}, ${this.age} years old`);
+}
+
+introduce.call(person1);
+introduce.call(person2);
