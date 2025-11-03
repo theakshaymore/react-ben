32ae31a5bcce351ddc33a2ab302a9eda1153cb0e
Akshay More <moreakshay725@gmail.com>
2025-11-03 10:58:02 +0530

    JS: call method example 2

--- 00__Concepts/JS/ployfills/call.js
+++ 00__Concepts/JS/ployfills/call.js
@@ -1,15 +1,36 @@
-let person1 = {
-  firstname: "akshay",
-  lastname: "more",
-};
-let person2 = {
-  firstname: "rohit",
-  lastname: "more",
-};
+//SECTION: EXAMPLE 1
+// // let person1 = {
+//   firstname: "akshay",
+//   lastname: "more",
+// };
+// let person2 = {
+//   firstname: "rohit",
+//   lastname: "more",
+// };
 
-let showFullName = function () {
-  console.log(`Hi my name is ${this.firstname} ${this.lastname}`);
-};
+// let showFullName = function () {
+//   console.log(`Hi my name is ${this.firstname} ${this.lastname}`);
+// };
 
-showFullName.call(person1);
-showFullName.call(person2);
+// showFullName.call(person1);
+// showFullName.call(person2);
+
+// SECTION: example 2
+
+function generateUserName(username) {
+  this.username = username;
+  console.log("called");
+}
+
+function createUser(username, email, password) {
+  generateUserName.call(this, username);
+  this.email = email;
+  this.password = password;
+}
+
+const user = new createUser("akshaymore", "akshay@gmail.com", "123");
+console.log(user);
+
+// usage
+// 1- method borrowing
+// 2- this binding
