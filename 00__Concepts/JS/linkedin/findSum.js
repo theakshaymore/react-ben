f1a29637b077bf483dda0b774e0c2503d9c7b5d4
Akshay More <moreakshay725@gmail.com>
2025-11-04 09:51:32 +0530

    JS: findsum using recursion

--- 00__Concepts/JS/linkedin/findSum.js
+++ 00__Concepts/JS/linkedin/findSum.js
@@ -2,4 +2,12 @@ let arr = [1, 2, 3, 4, 5];
 
 // using reduce()
 let res = arr.reduce((acc, val) => acc + val, 0);
-console.log(res);
+// console.log(res);
+
+// using recursion
+function getSum(arr, index = 0) {
+  if (index >= arr.length) return 0;
+  return arr[index] + getSum(arr, index + 1);
+}
+
+console.log(getSum(arr));
