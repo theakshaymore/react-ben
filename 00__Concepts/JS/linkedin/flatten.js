fc0440d6540283f2c6feb65c2994230bf201cb54
Akshay More <moreakshay725@gmail.com>
2025-11-04 08:34:24 +0530

    JS: arry flatten logic simple

--- 00__Concepts/JS/linkedin/flatten.js
+++ 00__Concepts/JS/linkedin/flatten.js
@@ -6,4 +6,20 @@ function flatten(arr) {
     []
   );
 }
-console.log(flatten([1, 2, 3, [4, 5, 6, [7, 8, [10, 11]]], 9]));
+// console.log(flatten([1, 2, 3, [4, 5, 6, [7, 8, [10, 11]]], 9]));
+
+function simpleFlatten(arr) {
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      res = res.concat(simpleFlatten(arr[i]));
+    } else {
+      res.push(arr[i]);
+    }
+  }
+
+  return res;
+}
+
+console.log(simpleFlatten([1, 2, 3, [4, 5, 6, [7, 8, [10, 11]]], 9]));
