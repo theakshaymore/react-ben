159ae360bac555201f4f92125277c96d32180b12
Akshay More <moreakshay725@gmail.com>
2025-11-13 10:10:03 +0530

    JS: filter method

--- 00__Concepts/JS/Sophisticated/Functions/higher-order-fun.js
+++ 00__Concepts/JS/Sophisticated/Functions/higher-order-fun.js
@@ -11,3 +11,10 @@ function sub(a, b) {
 
 higherFunction(5, 4, add);
 higherFunction(5, 4, sub);
+
+// filter method
+let nums = [2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const res = nums.filter((num) => num % 2 === 0);
+
+console.log(res);
