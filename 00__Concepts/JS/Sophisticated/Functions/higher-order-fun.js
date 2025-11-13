0ddb2c1373abbc60bcf61cc3caa52de4c7ca38a6
Akshay More <moreakshay725@gmail.com>
2025-11-13 10:14:34 +0530

    JS: methods of functions

--- 00__Concepts/JS/Sophisticated/Functions/higher-order-fun.js
+++ 00__Concepts/JS/Sophisticated/Functions/higher-order-fun.js
@@ -14,7 +14,12 @@ higherFunction(5, 4, sub);
 
 // filter method
 let nums = [2, 3, 4, 5, 6, 7, 8, 9, 0];
-
 const res = nums.filter((num) => num % 2 === 0);
-
 console.log(res);
+
+// reduce method
+const res2 = nums.reduce((acc, val) => acc + val, 0);
+console.log(res2);
+
+// forEach method
+console.log(nums.forEach((num) => console.log(num * 2)));
